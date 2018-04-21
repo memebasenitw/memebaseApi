3b37cbbde6222897e9753dbe6cde6de3e6194408
Rushabh Yapuram <rushabh.yapuram@gmail.com>
2018-04-21 15:09:11 +0530

    Initialize flask skeleton

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="Memebase API",
+    version="0.0.1",
+    description="Backend for memebase",
+    url="https://github.com/memebasenitw/memebaseApi",
+    packages=['api'],
+    install_requires=(
+        'flask'
+    )
+)
