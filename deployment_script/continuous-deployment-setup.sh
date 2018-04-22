e75045a7b44108325fd06220baa527bf491956d3
kaynite <sachin@pratilipi.com>
2018-04-23 01:01:38 +0530

    set up script added

---
+++ deployment_script/continuous-deployment-setup.sh
@@ -0,0 +1,11 @@
+sudo su
+cd ~
+
+apt-get update
+
+# https://cloud.google.com/sdk/downloads#apt-get
+apt-get install google-cloud-sdk
+apt-get install google-cloud-sdk-app-engine-java
+
+# Install required software
+apt-get install git
